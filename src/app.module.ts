0773c6c8178a188fcd8ccaeb9d39d7ca184a32ad
ztoc <bhu0444@gmail.com>
2024-11-06 03:13:40 -0500

    feat: added redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { HelloModule } from './hello/hello.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forRoot({
@@ -18,7 +20,12 @@ import { HelloModule } from './hello/hello.module';
     entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
     synchronize: true,
 
-  }), AuthModule, HelloModule],
+  }), AuthModule, HelloModule, CacheModule.register({
+    isGlobal: true,
+    store: redisStore,
+    host: 'redis',
+    port: 6379,
+  }),],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
